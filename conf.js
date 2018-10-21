d75fc07cd18d63a7cb2c21e9dad032736cff80fd
Zach <zacharyjordan82@gmail.com>
2018-10-21 15:30:25 -0400

    init

--- conf.js
+++ conf.js
@@ -2,7 +2,6 @@ exports.config = {
   framework: "jasmine",
   browserstackUser: "zachary108",
   browserstackKey: "qQJGfspsxYCVktpPa6EH",
-  seleniumAddress: "http://localhost:4444/wd/hub",
   specs: ["spec.js"],
   multiCapabilities: [
     {
--- spec.js
+++ spec.js
@@ -1,29 +1,8 @@
 // spec.js
 describe("Protractor Demo App", function() {
-  var firstNumber = element(by.model("first"));
-  var secondNumber = element(by.model("second"));
-  var goButton = element(by.id("gobutton"));
-  var latestResult = element(by.binding("latest"));
-  var history = element.all(by.repeater("result in memory"));
-
-  function add(a, b) {
-    firstNumber.sendKeys(a);
-    secondNumber.sendKeys(b);
-    goButton.click();
-  }
-
-  beforeEach(function() {
+  it("should have a title", function() {
     browser.get("http://juliemr.github.io/protractor-demo/");
-  });
 
-  it("should have a history", function() {
-    add(1, 2);
-    add(3, 4);
-
-    expect(history.count()).toEqual(2);
-
-    add(5, 6);
-
-    expect(history.count()).toEqual(0); // This is wrong!
+    expect(browser.getTitle()).toEqual("Super Calculator");
   });
 });
