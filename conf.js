63f4e0ba6e4a3be43c38941ab2b8a3f2c8a0ed6a
Zach <zacharyjordan82@gmail.com>
2018-10-21 15:27:30 -0400

    added key

--- conf.js
+++ conf.js
@@ -1,11 +1,10 @@
 exports.config = {
   framework: "jasmine",
+  browserstackUser: "zachary108",
+  browserstackKey: "qQJGfspsxYCVktpPa6EH",
   seleniumAddress: "http://localhost:4444/wd/hub",
   specs: ["spec.js"],
   multiCapabilities: [
-    {
-      browserName: "firefox"
-    },
     {
       browserName: "chrome"
     }
