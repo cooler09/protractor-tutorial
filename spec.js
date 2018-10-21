d23d5883147c0c75576c46aefcdba96abe6080b1
Zach <zacharyjordan82@gmail.com>
2018-10-21 15:33:42 -0400

    init

--- spec.js
+++ spec.js
@@ -3,6 +3,6 @@ describe("Protractor Demo App", function() {
   it("should have a title", function() {
     browser.get("http://juliemr.github.io/protractor-demo/");
 
-    expect(browser.getTitle()).toEqual("Super Calculator");
+    expect("test").toEqual("Super Calculator");
   });
 });
